27d9a1e939062bf4f2c2bdf548ff2b7b8af6a5d5
eric kertz <erickertz@gmail.com>
2014-10-27 10:26:39 -0400

    Initial commit of working roman numeral converter. We only use the most common roman numerals I, V, X, L, C, D, M and therefore can only convert in the range of 1 - 4999.
    All the work is done in the RomanNumeralConverter method which is passed
    an arabic numeral. We do a while loop until the arabic number is no
    longer greater than zero. Each interation of the loop checks the arabic
    number for the greatest possible match of a roman numeral. Once a match
    is found the number is appended to the main roman numeral conversion
    string and the value of the roman number is subtracted from the main
    arabic number. The loop then continues to its next iteration until the
    arabic number is completely broken down.
    
    There is also a range validator for good measure.

--- RomanNumeralConverter/src/main/java/com/urbn/interviewtest/RomanNumeralConverter.java
+++ RomanNumeralConverter/src/main/java/com/urbn/interviewtest/RomanNumeralConverter.java
@@ -2,33 +2,61 @@ package com.urbn.interviewtest;
 
 public class RomanNumeralConverter {
 
-	public String RomanNumeralConverter() {
+	public String RomanNumeralConverter(int NUMBER) {
+		String returnVal = "";
+		while(NUMBER > 0) {
+			if(NUMBER >= 1000) { returnVal += "M"; NUMBER -= 1000; continue; }
+			if(NUMBER >= 900) { returnVal += "CM"; NUMBER -= 900; continue; }
+			if(NUMBER >= 500) { returnVal += "D"; NUMBER -= 500; continue; }
+			if(NUMBER >= 400) { returnVal += "CD"; NUMBER -= 400; continue; }
+			if(NUMBER >= 100) { returnVal += "C"; NUMBER -= 100; continue; }
+			if(NUMBER >= 90) { returnVal += "XC"; NUMBER -= 90; continue; }
+			if(NUMBER >= 50) { returnVal += "L"; NUMBER -= 50; continue; }
+			if(NUMBER >= 40) { returnVal += "XL"; NUMBER -= 40; continue; }
+			if(NUMBER >= 10) { returnVal += "X"; NUMBER -= 10; continue; }
+			if(NUMBER >= 9) { returnVal += "IX"; NUMBER -= 9; continue; }
+			if(NUMBER >= 5) { returnVal += "V"; NUMBER -= 5; continue; }
+			if(NUMBER >= 4) { returnVal += "IV"; NUMBER -= 4; continue; }
+			if(NUMBER >= 1) { returnVal += "I"; NUMBER -= 1; continue; }
+		}
+		return returnVal;
+	}
+	
+	public boolean RomanNumeralValidator(int NUMBER) {
+		boolean returnVal = false;
+		if(NUMBER > 0 && NUMBER < 4999){
+			returnVal = true;
+		}
+		return returnVal;
 	}
 
 	public void problemOne() {
 		final int NUMBER = 56;
-		String solution = "";
-                
-		// TODO: Calculate the Roman Numeral equivalent of the value assigned to NUMBER
-
-		System.out.println("The roman numeral value of " + NUMBER + " is: " + solution);
+		if(this.RomanNumeralValidator(NUMBER)){
+			String solution = this.RomanNumeralConverter(NUMBER);
+			System.out.println("The roman numeral value of " + NUMBER + " is: " + solution);	
+		} else {
+			System.out.println("Number must be in the range of 1 - 4999");	
+		}
 	}
 
 	public void problemTwo() {
 		final int NUMBER = 763;
-		String solution = "";
-                
-		// TODO: Calculate the Roman Numeral equivalent of the value assigned to NUMBER
-
-		System.out.println("The roman numeral value of " + NUMBER + " is: " + solution);
+		if(this.RomanNumeralValidator(NUMBER)){
+			String solution = this.RomanNumeralConverter(NUMBER);
+			System.out.println("The roman numeral value of " + NUMBER + " is: " + solution);	
+		} else {
+			System.out.println("Number must be in the range of 1 - 4999");	
+		}
 	}
 
 	public void problemThree() {
 		final int NUMBER = 3195;
-		String solution = "";
-
-		// TODO: Calculate the Roman Numeral equivalent of the value assigned to NUMBER
-
-		System.out.println("The roman numeral value of " + NUMBER + " is: " + solution);
+		if(this.RomanNumeralValidator(NUMBER)){
+			String solution = this.RomanNumeralConverter(NUMBER);
+			System.out.println("The roman numeral value of " + NUMBER + " is: " + solution);	
+		} else {
+			System.out.println("Number must be in the range of 1 - 4999");	
+		}
 	}
 }
